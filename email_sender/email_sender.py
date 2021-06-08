dfe99a472020f6faf1866e8baacedae2909dada8
Epardo <emilio.p.221@gmail.com>
2021-06-08 17:49:23 +0200

    Automatic table reporting Finished cartridges sent through mail

--- email_sender/email_sender.py
+++ email_sender/email_sender.py
@@ -12,13 +12,15 @@ class Email:
         mail.To = receivers_list[0]
         mail.Subject = 'Report Cartuchos'
 
+        # Warning: do not remove the img html tag nor the attachment or the file will not be corrected sent
+
         mail.HTMLBody = """
         <p>Este correo se genera automáticamente.</p>
         <p>Los cartuchos fabricados por cada turno han sido:</p>
         <img src="TestFigure.png">
         """
 
-        # # To attach a file to the email (optional):
+        # To attach the image to be able to plot it:
         attachment = r'C:\Users\epardo\PycharmProjects\pythonProject\email_sender\TestFigure.png'
         mail.Attachments.Add(attachment)
 
